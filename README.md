5a6f1c204b3c0c685c5f1068f2fed8f986679019
Alief Aziz <serdralfs@gmail.com>
2018-08-22 11:16:36 +0700

    change db path to relative

--- README.md
+++ README.md
@@ -3,14 +3,34 @@
 move/cd to to your file destination quicker
 
 ## Usage : 
-1. Create a `context`
+1. Specify an empty folder path, it will be used to store the data
+
+    `TELEPORTATION_SYSTEM_DB_PATH="<enter the path here>"`
+
+2. Create a `context`
+
     `put_context <choose_a_name>`
-2. Use a `context`
+
+3. Use a `context`
+
     `use_context <your_choosen_name>`
-3. Bookmark a location
-    `bookmark`
-4. Go to your bookmarked location using `teleport`
+
+4. Bookmark a location
+
+    `bookmark 1`
+
+    (you can bookmark more than one place by just specifying the number)
+
+5. Go to your bookmarked location using `teleport`
+
     `teleport 1`
+
+## Additional Usage :
+
+1. blist : show list of bookmarked path
+2. clist : show list of created context
+3. clean_context_database : clear context database
+4. context : see help
 
 ## Installation :
 
@@ -24,5 +44,5 @@ GNU GPL
 
 
 ## TODO : 
-- Create code in go-lang, so this script could be used on any platform
+- Convert the script in go-lang, so this could be used on any platform
 
--- teleportation_system.sh
+++ teleportation_system.sh
@@ -1,6 +1,8 @@
 
 #----------------------------------------------------------------------------------- FEATURE - TELEPORTATION
 
+TELEPORTATION_SYSTEM_DB_PATH="$HOME/Documents/.script/database"
+
 # melist command fitur "TELEPORTATION" dan melihat konteks sekarang
 # untuk menggunakan fitur "TELEPORTATION", harus buat folder 
 function context(){
@@ -16,66 +18,66 @@ function context(){
   echo "bclear"
   echo "teleport"
 
-  global_context=$(cat $HOME/Documents/.script/database/teleporter_GLOBAL_CONTEXT)
-  # echo $(cat $HOME/Documents/.script/database/teleporter_GLOBAL_CONTEXT)
+  global_context=$(cat $TELEPORTATION_SYSTEM_DB_PATH/teleporter_GLOBAL_CONTEXT)
+  # echo $(cat $TELEPORTATION_SYSTEM_DB_PATH/teleporter_GLOBAL_CONTEXT)
   if [ ! -z "$local_context" ]; then
-    cat -n $HOME/Documents/.script/database/teleporter_$local_context
+    cat -n $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$local_context
   else
-    cat -n $HOME/Documents/.script/database/teleporter_$global_context
+    cat -n $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$global_context
   fi
 }
 
 # membuat konteks baru 
 # cara pakai : `put_context _nama_konteks(string)_`
 function put_context(){
-  if [ ! -d "$HOME/Documents/.script/database" ]; then
-    mkdir -p "$HOME/Documents/.script/database";
+  if [ ! -d "$TELEPORTATION_SYSTEM_DB_PATH" ]; then
+    mkdir -p "$TELEPORTATION_SYSTEM_DB_PATH";
   fi
-  list_manager $HOME/Documents/.script/database/teleporter_CONTEXT $1 $2 
-  touch $HOME/Documents/.script/database/teleporter_$1
+  list_manager $TELEPORTATION_SYSTEM_DB_PATH/teleporter_CONTEXT $1 $2 
+  touch $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$1
 }
-alias clist="cat -n $HOME/Documents/.script/database/teleporter_CONTEXT"
-# alias cclear="echo > $HOME/Documents/.script/database/teleporter_CONTEXT; 
-# rm $HOME/Documents/.script/database/teleporter_*'
+alias clist="cat -n $TELEPORTATION_SYSTEM_DB_PATH/teleporter_CONTEXT"
+# alias cclear="echo > $TELEPORTATION_SYSTEM_DB_PATH/teleporter_CONTEXT; 
+# rm $TELEPORTATION_SYSTEM_DB_PATH/teleporter_*'
 
 
 # mengguks yang sudah ada 
 # cara pakai : `use_context _nama_konteks(string)_`
 function use_context(){
-  if [ ! -d "$HOME/Documents/.script/database" ]; then
-    mkdir -p "$HOME/Documents/.script/database";
+  if [ ! -d "$TELEPORTATION_SYSTEM_DB_PATH" ]; then
+    mkdir -p "$TELEPORTATION_SYSTEM_DB_PATH";
   fi
   if [ "$#" -lt 1 ]; then
     local num=1
   else
     local num=$1
   fi
-  local_context=$(sed "${num}q;d;" $HOME/Documents/.script/database/teleporter_CONTEXT)
+  local_context=$(sed "${num}q;d;" $TELEPORTATION_SYSTEM_DB_PATH/teleporter_CONTEXT)
   # set global context
   if [ "$#" -gt 1 ]; then 
-    echo "$local_context">$HOME/Documents/.script/database/teleporter_GLOBAL_CONTEXT
+    echo "$local_context">$TELEPORTATION_SYSTEM_DB_PATH/teleporter_GLOBAL_CONTEXT
     echo "\"$local_context\" diset menjadi global context "
   fi
   echo "context \"$local_context\" telah siap"
 }
 
 # function set_global_context(){
-# if [ -z $context ]; then context=$(cat $HOME/Documents/.script/database/teleporter_GLOBAL_CONTEXT); fi
+# if [ -z $context ]; then context=$(cat $TELEPORTATION_SYSTEM_DB_PATH/teleporter_GLOBAL_CONTEXT); fi
 
 # membersihkan database context
 # cara pakai : `clean_context_database`
 function clean_context_database(){
-  if [ ! -d "$HOME/Documents/.script/database" ]; then
-    mkdir -p "$HOME/Documents/.script/database";
+  if [ ! -d "$TELEPORTATION_SYSTEM_DB_PATH" ]; then
+    mkdir -p "$TELEPORTATION_SYSTEM_DB_PATH";
   fi
-  for ii in $(ls $HOME/Documents/.script/database/ | grep teleporter_) ; do
+  for ii in $(ls $TELEPORTATION_SYSTEM_DB_PATH | grep teleporter_) ; do
     local hapus=1
     while read jj; do
       # jika ii ga ada di list, hapus
       if [ ! -z $jj ] && echo $ii | grep "$jj\|CONTEXT" > /dev/null; then
         local hapus=0
       fi
-    done < "$HOME/Documents/.script/database/teleporter_CONTEXT"
+    done < "$TELEPORTATION_SYSTEM_DB_PATH/teleporter_CONTEXT"
     if [ $hapus -eq 1 ]; then
       rm $ii
     fi
@@ -86,53 +88,53 @@ function clean_context_database(){
 # menyimpan current path dengan index tertentu ke konteks yang sedang dipakai  
 # cara pakai : `bookmark _index(int)_`
 function bookmark(){
-  if [ ! -d "$HOME/Documents/.script/database" ]; then
-    mkdir -p "$HOME/Documents/.script/database";
+  if [ ! -d "$TELEPORTATION_SYSTEM_DB_PATH" ]; then
+    mkdir -p "$TELEPORTATION_SYSTEM_DB_PATH";
   fi
   local path=$(pwd | sed 's/\//\\\//g') 
   #echo $path
-  list_manager $HOME/Documents/.script/database/teleporter_$global_context "$path" $1
+  list_manager $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$global_context "$path" $1
 }
 
 # melihat daftar path pada current context
 # cara pakai : `blist`
 function blist(){
-  if [ ! -d "$HOME/Documents/.script/database" ]; then
-    mkdir -p "$HOME/Documents/.script/database";
+  if [ ! -d "$TELEPORTATION_SYSTEM_DB_PATH" ]; then
+    mkdir -p "$TELEPORTATION_SYSTEM_DB_PATH";
   fi
-  global_context=$(cat $HOME/Documents/.script/database/teleporter_GLOBAL_CONTEXT)
-  # echo $(cat $HOME/Documents/.script/database/teleporter_GLOBAL_CONTEXT)
+  global_context=$(cat $TELEPORTATION_SYSTEM_DB_PATH/teleporter_GLOBAL_CONTEXT)
+  # echo $(cat $TELEPORTATION_SYSTEM_DB_PATH/teleporter_GLOBAL_CONTEXT)
 
-  if [ -z $HOME/Documents/.script/database/teleporter_$global_context ]; then 
+  if [ -z $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$global_context ]; then 
     echo "context global / local belum di set"
   else
     if [ ! -z "$local_context" ]; then
-      cat -n $HOME/Documents/.script/database/teleporter_$local_context
+      cat -n $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$local_context
     else
-      cat -n $HOME/Documents/.script/database/teleporter_$global_context
+      cat -n $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$global_context
     fi
   fi
 }
-alias bclear="echo > $HOME/Documents/.script/database/teleporter_$context"
+alias bclear="echo > $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$context"
 
 # cd ke path yang ada di blist 
 # cara pakai : `teleport 2`, cd ke path yang ada di index ke 2 pada blist
 function teleport(){
-  if [ ! -d "$HOME/Documents/.script/database" ]; then
-    mkdir -p "$HOME/Documents/.script/database";
+  if [ ! -d "$TELEPORTATION_SYSTEM_DB_PATH" ]; then
+    mkdir -p "$TELEPORTATION_SYSTEM_DB_PATH";
   fi
   if [ "$#" -ne 1 ]; then
     local num=1
   else
     local num=$1
   fi
-  global_context=$(cat $HOME/Documents/.script/database/teleporter_GLOBAL_CONTEXT)
-  # echo $(cat $HOME/Documents/.script/database/teleporter_GLOBAL_CONTEXT)
+  global_context=$(cat $TELEPORTATION_SYSTEM_DB_PATH/teleporter_GLOBAL_CONTEXT)
+  # echo $(cat $TELEPORTATION_SYSTEM_DB_PATH/teleporter_GLOBAL_CONTEXT)
 
   if [ ! -z "$local_context" ]; then
-    local path=$(sed "${num}q;d;" $HOME/Documents/.script/database/teleporter_$local_context)
+    local path=$(sed "${num}q;d;" $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$local_context)
   else
-    local path=$(sed "${num}q;d;" $HOME/Documents/.script/database/teleporter_$global_context)
+    local path=$(sed "${num}q;d;" $TELEPORTATION_SYSTEM_DB_PATH/teleporter_$global_context)
   fi
 
 
