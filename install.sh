48ca3a4751f8d8a88933d0793843b5221b7b9b97
Alief <serdralfs@gmail.com>
2019-02-04 16:57:29 +0700

    refactor: improve installation script

--- install.sh
+++ install.sh
@@ -3,19 +3,25 @@ mkdir -p "$HOME/Documents/.script/database"
 installer_path="$(dirname ${BASH_SOURCE[0]})"
 destination_path="$HOME/Documents/.script"
 
-# register the script for auto source
-if [[ -z "$(cat $HOME/.bashrc | grep teleportation_system)" ]]; then 
+# copy the script 
+if [[ -z "$(ls $HOME/Documents/.script | grep teleportation_system\.sh)" ]]; then 
   # copy source
   "cp" "$installer_path/teleportation_system.sh" "$destination_path"
   echo "Copying done" : "cp $installer_path/teleportation_system.sh" "$destination_path"
+else # case if already installed
+  echo 
+  echo "Teleportation system executable is already exist"
+  echo
+fi
 
+# register the script for auto source
+if [[ -z "$(cat $HOME/.bashrc | grep teleportation_system)" ]]; then 
   # add auto source script
   echo "# teleportation_system" >> $HOME/.bashrc
   "echo" "source $HOME/Documents/.script/teleportation_system.sh" >> $HOME/.bashrc
   echo "registering to bashrc done"
-
 else # case if already installed
   echo 
-  echo "Teleportation system is already installed"
+  echo "Teleportation system auto source is already defined"
   echo
 fi
